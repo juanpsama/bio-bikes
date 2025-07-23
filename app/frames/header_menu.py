6bf5f6a246ea4e61a55cbb9183d22b95ccf4d6d1
juanpsama <samajpablo@gmail.com>
2025-07-23 13:06:37 -0600

    feat: update the menu frame

--- app/frames/header_menu.py
+++ app/frames/header_menu.py
@@ -1,45 +1,39 @@
 from tkinter import Menu, Tk
 
+
 class HeaderMenu(Menu):
-    def __init__(self, parent: Tk, controller):
+    def __init__(self, parent: Tk, controller, menu_frames):
         super().__init__(parent)
         self.controller = controller
-        parent.config(menu=self)
+        # parent.config(menu=self)
 
         # Create the File menu
         file_menu = Menu(self, tearoff=0)
-        self.add_cascade(label="File", menu=file_menu)
-        file_menu.add_command(label="New...", command=self.new_command)
-        file_menu.add_separator()
+        self.add_cascade(label="App", menu=file_menu)
         file_menu.add_command(label="Exit", command=self.exit_app)
 
         # Create the Navigation menu
         navigation_menu = Menu(self, tearoff=0)
-        self.add_cascade(label="Navigation", menu=navigation_menu)
-        options_list = [
-            "SelectPacientFrame",
-            "PacientDataFrame",
-            "SelectImageInputFrame",
-            "VideoAnalisisFrame",
-            "WebcamImageInputFrame",
-            "PacientInfoFrame"
-        ]
-        for option in options_list:
-            navigation_menu.add_command(label=option, command=lambda opt=option: self.navigate(opt))
+        self.add_cascade(label="Menú", menu=navigation_menu)
+        for key, value in menu_frames.items():
+            navigation_menu.add_command(
+                label=value.display_name or key,
+                command=lambda opt=key: self.navigate(opt),
+            )
 
-        # Create the Configuration menu
-        config_menu = Menu(self, tearoff=0)
-        self.add_cascade(label="Configuration", menu=config_menu)
-        config_options_list = [
-            "Config Option 1",
-            "Config Option 2",
-            "Config Option 3"
-        ]
-        for option in config_options_list:
-            config_menu.add_command(label=option, command=lambda opt=option: self.open_config(opt))
+        # # Create the Configuration menu
+        # config_menu = Menu(self, tearoff=0)
+        # self.add_cascade(label="Configuration", menu=config_menu)
+        # config_options_list = [
+        #     "Config Option 1",
+        #     "Config Option 2",
+        #     "Config Option 3"
+        # ]
+        # for option in config_options_list:
+        #     config_menu.add_command(label=option, command=lambda opt=option: self.open_config(opt))
 
-        # Create the Logout menu
-        self.add_command(label="Logout", command=self.logout)
+        # # Create the Logout menu
+        # self.add_command(label="Logout", command=self.logout)
 
     def new_command(self):
         # Placeholder for new command logic
